739f5a6ca9b840a3982a8f214399ad5e0be2f6c9
marco <marco@santana.nyc>
2023-04-28 17:01:12 -0400

    split to functions and BigInt incorporated

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,27 @@
- use num_bigint::{BigInt, Sign};
+ use num_bigint::{BigInt, Sign, ToBigInt};
 // use num_traits::Pow;
-use std::time::Instant;
+// use std::time::Instant;
 
 fn main() {
     // let pi: f64 = leibniz(n);
     // println!("Calculation took {} minutes.", elapsed_time.as_secs()/60);
     // println!("Hello, Pi is {:.*}", 20, pi);
-    for i in 0..=2 {
-        let now = Instant::now();
-        println!("chudnovsky({}): {:.*}", i, 20, chudnovsky(i));
-        let elapsed_time = now.elapsed();
-        println!("{} microsecs", elapsed_time.as_micros());
-    }
+    // for i in 0..=2 {
+        // let now = Instant::now();
+        // println!("chudnovsky({}): {:.*}", i, 20, chudnovsky(i));
+        // let elapsed_time = now.elapsed();
+        // println!("{} microsecs", elapsed_time.as_micros());
+    // }
+    let mut big_int: BigInt = ToBigInt::to_bigint(&1).unwrap();
+    let Constant: f64 = 426880.0 * 10005.0_f64.sqrt();
+    let q = 2;
+    big_int = chud_exponential(q);
+    println!("chud_exponential({}) = {}", q, big_int);
+    big_int = chud_linear(q);
+    println!("chud_linear({}) = {}", q, big_int);
+    big_int = chud_multinomial(q);
+    println!("chud_multinomial({}) = {}", q, big_int);
+    
 }
 
 /**
@@ -46,13 +56,15 @@ fn chudnovsky(i: i128) -> f64 {
     /* overflow problem now */
     // can we utilize u64 up until the bitter end?
     // let base: BigInt = BigInt::new( Sign::Minus, Vec::from([2,6,2,5,3,7,4,1,2,6,4,0,7,6,8,0,0,0]) );
+/*
     let base: i128 = -262537412640768000;
     let C: f64 = 426880.0 * 10005.0_f64.sqrt();
     let mut multinomial: i128 = 0;
     let mut linear: i128 = 0;
     let mut exponential: i128 = 1; // BigInt = BigInt::new( Sign::Plus, Vec::new() );
     let mut pi: i128 = 0;
-
+*/
+/*
     // infinite sum
     for q in 0..i {
         exponential = base.pow(i as u32); // Pow::pow( base, i as u32);
@@ -65,39 +77,34 @@ fn chudnovsky(i: i128) -> f64 {
         println!("pi {}: {}", n, pi);
     }
     C * pi as f64
+*/
+    0.0 as f64
 }
 
 /* linear = 545140134q + 13591409 */
 // usize for now since its ran in a for loop
 fn chud_linear(q: usize) -> BigInt { 
-    result_at_q = BigInt::new( Sign::Plus, Vec::from[5,4,5,1,4,0,1,3,4]);
-    reslut_at_q *= q;
-    reslut_at_q += 13591409;
+    let mut result_at_q = BigInt::new( Sign::Plus, Vec::from([5,4,5,1,4,0,1,3,4]) );
+    result_at_q *= q;
+    result_at_q += 13591409;
     result_at_q
 }
 
 /* exponential = -262537412640768000^q */
 // usize for now since its ran in a forloop
 fn chud_exponential(q: usize) -> BigInt { 
-    // quick and dirty.. there should be a lib doing this for me,
-    // but I will just impl my own until the compiler yells at me.
-    let base: BigInt = BigInt::new( Sign::Minus, Vec::from([2,6,2,5,3,7,4,1,2,6,4,0,7,6,8,0,0,0]) );
-    let mut result: BigInt = BigInt::new(Sign::Plus, 1); // base^0
-    
-    // exponentiation!
-    for i in 0..q { result *= base; } // base * base q times
-    result
+    BigInt::new( Sign::Minus, Vec::from([2,6,2,5,3,7,4,1,2,6,4,0,7,6,8,0,0,0]) ).pow(q as u32)
 }
 
 /* multinomial = (6q)! / (3q)!(q!)^3 */
 // usize for now since its ran in a forloop
 fn chud_multinomial(q: usize) -> BigInt { 
-        multinomial = factorial(6*n) /
-        ( factorial(3*n) * factorial(n).pow(3) );
+        /* (6q)! */         /* (3q)! */         /* (q!)^3 */
+    factorial(6 * q) / ( factorial(3 * q) * (factorial(q)).pow(3) )
 }
 
 fn factorial(n: usize) -> BigInt {
-    let mut fact_that_shit: BigInt = BigInt::new(Sign::Plus, 1);
+    let mut fact_that_shit: BigInt = ToBigInt::to_bigint(&1).unwrap();
     for i in 1..=n { fact_that_shit *= i; }
     fact_that_shit
 }
